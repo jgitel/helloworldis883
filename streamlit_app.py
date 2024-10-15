f54fad0064ebb3ac7d25fa8cd32f65bd1aa9e92b
jgitel <156343109+jgitel@users.noreply.github.com>
2024-10-15 16:17:35 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,28 @@
-import streamlit as st
+import streamlit as st from openai import OpenA
+import os
 
 st.title("🎈 Welcome to Hell")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+#* Load your API Key
+my_secret_key = userdata.get ('MyOpenAIKey')
+os. environ ["OPENAI_API_KEY"] = "asasasaas")
+#** Request the answer to the question "Damascus is a"
+client = OpenAI()
+response = client.chat.completions.create(
+model="gpt -40-mini", messages=[
+("role": "system", "content": "Complete the following prefix"), ("role": "user", "content": "Damascus is a"}
+seed = BUID,
+กะ 10,
+max_tokens=20
++
+S
+Cancel changes
+Commit changes.
+Spaces
+4
+No wrap
+Use
+was Print all 10 complatione:
